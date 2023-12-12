aaa979108373a8c56d5895dca2d1e20d71bca92d
ncteam1990 <mailusqueries@gmail.com>
2023-12-12 11:12:35 +0530

    check in

---
+++ src/main/java/com/ncteam/democopilot/DemoCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.ncteam.democopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCopilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncteam/democopilot/DemoCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncteam.democopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
